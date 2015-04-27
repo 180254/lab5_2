5ec0f601980f484535331b7ce21d0e0ec4d33c63
Adrian Pędzwiatr <180254@edu.p.lodz.pl>
2015-04-27 10:16:55 +0200

    annotated ApplicationConfig as spring configuration

--- src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
+++ src/main/java/edu/iis/mto/integrationtest/config/ApplicationConfig.java
@@ -1,8 +1,12 @@
 package edu.iis.mto.integrationtest.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-
+@Configuration
+@ComponentScan("edu.iis.mto.integrationtest.repository")
+@Import(value = { PersistenceConfig.class })
 public class ApplicationConfig {
 
-	
 }
