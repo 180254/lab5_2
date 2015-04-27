d394c55dcc5d3bf896beb295d6c762a80cd0300f
Adrian Pędzwiatr <180254@edu.p.lodz.pl>
2015-04-27 10:20:50 +0200

    Annotated PersistenceConfig as spring persistance configuration

--- src/main/java/edu/iis/mto/integrationtest/config/PersistenceConfig.java
+++ src/main/java/edu/iis/mto/integrationtest/config/PersistenceConfig.java
@@ -1,8 +1,12 @@
 package edu.iis.mto.integrationtest.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = { "edu.iis.mto.integrationtest.repository" })
 public class PersistenceConfig {
-	
 
 }
